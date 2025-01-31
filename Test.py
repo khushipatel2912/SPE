7f23b0633860eed19d6f3093c079fe6158fc5665
khushipatel2912 <67056307+khushipatel2912@users.noreply.github.com>
2025-01-31 18:12:01 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,9 +9,9 @@ class TestSum(unittest.TestCase):
         """
         Test case to add two numbers
         """
-        data = [30, 31]
+        data = [30, 30]
         result = summation(data)
-        self.assertEqual(result, 61)
+        self.assertEqual(result, 60)
 
 if __name__ == '__main__':
     unittest.main()
